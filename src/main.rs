642e902c2da1af23c274f17c8079309b326274e3
Tim Van Wassenhove <tim@timvw.be>
2022-01-08 10:27:21 +0100

    get started with rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+#[allow(unused_must_use)]
+async fn main() {
+    rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await;
 }
