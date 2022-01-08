ad81d1a72c71e212cc62c2062b05b29f18cc4fb0
Tim Van Wassenhove <tim@timvw.be>
2022-01-08 17:28:44 +0100

    some work to parse graphql query response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #[macro_use] extern crate rocket;
 
+use serde::{Serialize, Deserialize};
+use chrono::prelude::*;
+use ics::properties::{Categories, Description, DtEnd, DtStart, Location, Organizer, Status, Summary};
+use ics::{escape_text, Event, ICalendar};
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
@@ -14,8 +19,6 @@ async fn main() {
         .await;
 }
 
-use chrono::prelude::*;
-
 fn format_datetime(dt: DateTime<Utc>) -> String {
     dt.format("%Y%m%dT%H%M%SZ").to_string()
 }
@@ -25,9 +28,6 @@ fn can_generate_dtstamp() {
     let dt: DateTime<Utc> = Utc.ymd(2022, 1, 2).and_hms(3, 4, 5);
     assert_eq!(format_datetime(dt), "20220102T030405Z");
 }
-
-use ics::properties::{Categories, Description, DtEnd, DtStart, Location, Organizer, Status, Summary};
-use ics::{escape_text, Event, ICalendar};
 
 fn make_event<'a>(uid: &'a str, description: &'a str, start: DateTime<Utc>, end: DateTime<Utc>, location: Option<&'a str>) -> Event<'a> {
     let mut event = Event::new(uid, format_datetime(Utc::now()));
@@ -56,4 +56,143 @@ fn can_generate_calendar() {
 
     calendar.write(std::io::stdout()).expect("Failed to write calendar");
     calendar.save_file("/tmp/test.ics").expect("Failed to save calendar");
+}
+
+#[tokio::test]
+async fn can_fetch_team_calendar() {
+
+    let team_id = 22235414;
+
+    let url = format!("https://datalake-prod2018.rbfa.be/graphql?operationName=GetTeamCalendar&variables=%7B%22teamId%22%3A%{}%22%2C%22language%22%3A%22nl%22%2C%22sortByDate%22%3A%22asc%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22bf4be0c185dee11a27079e529a04d41dc692389ada678dac1f2280e056de7b7d%22%7D%7D", team_id);
+
+    let resp = reqwest::get(url)
+        .await.unwrap()
+        .json::<serde_json::Value>()
+        .await.unwrap();
+
+    println!("{:?}", resp);
+
+    assert!(true)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TeamCalendarResponse {
+    data: TeamCalendarmResponseData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TeamCalendarmResponseData {
+    #[serde(rename = "teamCalendar")]
+    team_calendar: Vec<MatchDetail>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MatchDetail {
+    id: String,
+    #[serde(rename = "startDate")]
+    start_date: String,
+    channel: String,
+    #[serde(rename = "homeTeam")]
+    home_team: MatchDetailTeam,
+    #[serde(rename = "awayTeam")]
+    away_team: MatchDetailTeam,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MatchDetailTeam {
+    id: String,
+    name: String,
+    #[serde(rename = "clubId")]
+    club_id: String,
+    logo: String,
+    #[serde(rename = "__typename")]
+    type_name: String,
+}
+
+#[test]
+fn can_parse_team_calendar() {
+
+    let input = r#"
+{
+    "data": {
+        "teamCalendar": [
+            {
+                "id": "5584787",
+                "startDate": "2021-09-04T14:00:00",
+                "channel": "voetbalvlaanderen",
+                "homeTeam": {
+                    "id": "234947",
+                    "name": "KVZ.Glabbeek B 1",
+                    "clubId": "2028",
+                    "logo": "https://belgianfootball.s3.eu-central-1.amazonaws.com/s3fs-public/rbfa/img/logos/clubs/05806.jpg",
+                    "__typename": "MatchDetailTeam"
+                },
+                "awayTeam": {
+                    "id": "235412",
+                    "name": "VK.Linden B 2-1",
+                    "clubId": "2725",
+                    "logo": "https://belgianfootball.s3.eu-central-1.amazonaws.com/s3fs-public/rbfa/img/logos/clubs/08522.jpg",
+                    "__typename": "MatchDetailTeam"
+                },
+                "outcome": {
+                    "status": "planned",
+                    "homeTeamGoals": null,
+                    "homeTeamPenaltiesScored": null,
+                    "awayTeamGoals": null,
+                    "awayTeamPenaltiesScored": null,
+                    "forfeitBy": "",
+                    "subscript": null,
+                    "__typename": "MatchDetailOutcome"
+                },
+                "series": {
+                    "id": "CHP_99506",
+                    "name": "GEW. U12 -I-",
+                    "__typename": "MatchSeries"
+                },
+                "officials": [],
+                "__typename": "MatchDetail"
+            },
+            {
+                "id": "5661724",
+                "startDate": "2021-09-11T11:30:00",
+                "channel": "voetbalvlaanderen",
+                "homeTeam": {
+                    "id": "237896",
+                    "name": "KDN.UNITED",
+                    "clubId": "2539",
+                    "logo": "https://belgianfootball.s3.eu-central-1.amazonaws.com/s3fs-public/rbfa/img/logos/clubs/07758.jpg",
+                    "__typename": "MatchDetailTeam"
+                },
+                "awayTeam": {
+                    "id": "235412",
+                    "name": "VK.LINDEN",
+                    "clubId": "2725",
+                    "logo": "https://belgianfootball.s3.eu-central-1.amazonaws.com/s3fs-public/rbfa/img/logos/clubs/08522.jpg",
+                    "__typename": "MatchDetailTeam"
+                },
+                "outcome": {
+                    "status": "planned",
+                    "homeTeamGoals": null,
+                    "homeTeamPenaltiesScored": null,
+                    "awayTeamGoals": null,
+                    "awayTeamPenaltiesScored": null,
+                    "forfeitBy": "",
+                    "subscript": null,
+                    "__typename": "MatchDetailOutcome"
+                },
+                "series": {
+                    "id": "FRN_178",
+                    "name": "U12",
+                    "__typename": "MatchSeries"
+                },
+                "officials": [],
+                "__typename": "MatchDetail"
+            }
+        ]
+    }
+}
+    "#;
+
+    let resp: TeamCalendarResponse = serde_json::from_str(input).unwrap();
+    println!("{:?}", resp);
 }
