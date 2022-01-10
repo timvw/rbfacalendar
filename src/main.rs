bdb1e86c36eeea9392bff2634546d2e497c9a21c
Tim Van Wassenhove <tim@timvw.be>
2022-01-10 21:25:29 +0100

    remove rust index page and rely on angular index

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,16 @@ mod calendar;
 mod rbfa;
 
 use rocket::http::ContentType;
-use rocket::response::content;
 use rocket::fs::{FileServer, relative};
 
 #[rocket::main]
 #[allow(unused_must_use)]
 async fn main() {
     rocket::build()
-        .mount("/", routes![index, calendar_for_team_id])
+        .mount("/", routes![calendar_for_team_id])
         .mount("/", FileServer::from(relative!("./webapp/dist/webapp")))
         .launch()
         .await;
-}
-
-#[get("/")]
-fn index() -> content::Html<&'static str> {
-    content::Html(r#"
-    <ul>
-    <li><a href='/index.html'>Angular</a></li>
-    <li><a href='/calendar/22235414'>calendar for 22235414</a></li>
-    </ul>
-    "#)
 }
 
 #[get("/calendar/<team_id>")]
