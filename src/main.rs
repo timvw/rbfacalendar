e8376c55b592020c8ce88fa3642b0fc426172d83
Tim Van Wassenhove <tim@timvw.be>
2022-01-10 17:51:12 +0100

    use fileserver instead of manually implementing static files

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ extern crate rocket;
 mod calendar;
 mod rbfa;
 
-use std::path::{Path, PathBuf};
-use rocket::fs::NamedFile;
 use rocket::http::ContentType;
 use rocket::response::content;
+use rocket::fs::{FileServer, relative};
 
 #[rocket::main]
 #[allow(unused_must_use)]
 async fn main() {
     rocket::build()
-        .mount("/", routes![index, calendar_for_team_id, assets])
+        .mount("/", routes![index, calendar_for_team_id])
+        .mount("/", FileServer::from(relative!("/static/assets")))
         .launch()
         .await;
 }
@@ -21,11 +21,6 @@ async fn main() {
 #[get("/")]
 fn index() -> content::Html<&'static str> {
     content::Html("<a href='/calendar/22235414'>22235414</a>")
-}
-
-#[get("/<path..>")]
-async fn assets(path: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/assets").join(path)).await.ok()
 }
 
 #[get("/calendar/<team_id>")]
