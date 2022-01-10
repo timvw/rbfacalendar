2c6997665fbb4892c29816cbf1b129865b6a9a1b
Tim Van Wassenhove <tim@timvw.be>
2022-01-10 12:05:42 +0100

    cleanup some unused code

--- src/calendar.rs
+++ src/calendar.rs
@@ -4,54 +4,28 @@ use chrono::{DateTime, Utc};
 use chrono::prelude::*;
 use crate::rbfa::MatchDetail;
 
-fn format_datetime(dt: DateTime<Utc>) -> String {
+fn format_as_dtstamp(dt: DateTime<Utc>) -> String {
     dt.format("%Y%m%dT%H%M%SZ").to_string()
 }
 
 #[test]
-fn can_generate_dtstamp() {
-    assert_eq!(format_datetime(Utc.ymd(2022, 1, 2).and_hms(3, 4, 5)), "20220102T030405Z");
+fn can_format_as_dtstamp() {
+    assert_eq!(format_as_dtstamp(Utc.ymd(2022, 1, 2).and_hms(3, 4, 5)), "20220102T030405Z");
 }
 
-fn make_event<'a>(uid: &'a str, description: &'a str, start: DateTime<Utc>, end: DateTime<Utc>, location: Option<&'a str>) -> Event<'a> {
-    let mut event = Event::new(uid, format_datetime(Utc::now()));
-    event.push(DtStart::new(format_datetime(start)));
-    event.push(DtEnd::new(format_datetime(end)));
-    event.push(Description::new(escape_text(description)));
-    match location {
-        Some(x) => event.push(Location::new(x)),
-        _ => {}
-    }
+fn make_match_detail_event(match_detail: &MatchDetail) -> Event<'_> {
+    let mut event = Event::new(&match_detail.id, format_as_dtstamp(Utc::now()));
+    event.push(DtStart::new(format_as_dtstamp(match_detail.start_date)));
+    event.push(DtEnd::new(format_as_dtstamp(match_detail.start_date+ chrono::Duration::hours(2))));
+    event.push(Summary::new(escape_text(format!("{} - {}", match_detail.home_team.name, match_detail.away_team.name))));
+    event.push(Description::new(escape_text(format!("{} - {}", match_detail.home_team.name, match_detail.away_team.name))));
     event
 }
 
-fn make_match_event<'a>(description: &'a str, start: DateTime<Utc>, location: Option<&'a str>) -> Event<'a> {
-    make_event(description, description, start, start + chrono::Duration::hours(2), location)
-}
-
-#[test]
-#[ignore]
-fn can_generate_calendar() {
-    let mut calendar = ICalendar::new("2.0", "-//xyz Corp//NONSGML PDA Calendar Version 1.0//EN");
-
-    calendar.add_event(make_match_event(
-        "VK.Linden 3 - K.Ol.Wijgmaal B 3",
-        Utc.ymd(2022, 1, 8).and_hms(3,2,3),
-        Some("Pastoor Bellonstraat 23, 3018 Leuven")));
-
-    calendar.write(std::io::stdout()).expect("Failed to write calendar");
-    calendar.save_file("/tmp/test.ics").expect("Failed to save calendar");
-}
-
-pub fn make_calendar_from_rbfa_match_details<'a>(match_details: &'a Vec<MatchDetail>) -> ICalendar<'a> {
+pub fn make_calendar_from_rbfa_match_details(match_details: &Vec<MatchDetail>) -> ICalendar {
     let mut calendar = ICalendar::new("2.0", "-//xyz Corp//NONSGML PDA Calendar Version 1.0//EN");
     for match_detail in match_details {
-        let mut event = Event::new(&match_detail.id, format_datetime(Utc::now()));
-        event.push(DtStart::new(format_datetime(match_detail.start_date)));
-        event.push(DtEnd::new(format_datetime(match_detail.start_date+ chrono::Duration::hours(2))));
-        event.push(Summary::new(escape_text(format!("{} - {}", match_detail.home_team.name, match_detail.away_team.name))));
-        event.push(Description::new(escape_text(format!("{} - {}", match_detail.home_team.name, match_detail.away_team.name))));
-        calendar.add_event(event)
+        calendar.add_event(make_match_detail_event(&match_detail));
     }
     calendar
 }
