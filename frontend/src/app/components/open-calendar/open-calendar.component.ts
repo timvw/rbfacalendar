81811f50139405474ed61df38d22603604f0785c
Tim Van Wassenhove <tim@timvw.be>
2022-03-05 09:42:43 +0100

    make link to open calendar work again

--- frontend/src/app/components/open-calendar/open-calendar.component.ts
+++ frontend/src/app/components/open-calendar/open-calendar.component.ts
@@ -8,38 +8,22 @@ import {Club} from "../../models/club";
   templateUrl: './open-calendar.component.html',
   styleUrls: ['./open-calendar.component.css']
 })
-export class OpenCalendarComponent implements OnInit, OnChanges {
+export class OpenCalendarComponent {
 
   @Input() club!: Club;
   @Input() team!: Team;
-
-  title: string = '';
-  calendar_url: SafeResourceUrl = '';
 
   constructor(
     private sanitizer: DomSanitizer
   ) { }
 
-  ngOnInit(): void {
-    this.assignData()
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.assignData()
-  }
-
-  assignData() {
-    this.title = this.calculateTitle();
-    this.calendar_url = this.calculateCalendarUrl();
-  }
-
   calculateTitle() {
     return this.club.name + " - " + this.team.name;
   }
 
-  calculateCalendarUrl(): SafeResourceUrl {
+  get calendarUrl(): SafeResourceUrl {
     const url = new URL("webcal://" + window.location.host + "/api/calendar/" + encodeURIComponent(this.team.team_id));
-    url.searchParams.append("title", this.title);
+    url.searchParams.append("title", this.calculateTitle());
     return this.sanitizer.bypassSecurityTrustResourceUrl(url.href);
   }
 
